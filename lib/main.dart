3674b602426caa5f82da6f6c962f26c260f75a14
Avinandan Bose <38869235+AvinandanBose@users.noreply.github.com>
2022-04-29 21:56:55 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat_flutter_firebase/screens/welcome_screen.dart';
+import 'package:flash_chat_flutter_firebase/screens/login_screen.dart';
+import 'package:flash_chat_flutter_firebase/screens/registration_screen.dart';
+import 'package:flash_chat_flutter_firebase/screens/chat_screen.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
